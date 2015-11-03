a8358fd233182dd50f8506d78cca58991d7c57ed
Jesus Imery <yisas_0929@hotmail.com>
2015-11-03 11:27:32 -0500

    login page created, working routes navbar

--- website/app/controllers/static_pages_controller.rb
+++ website/app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
 
-  def help
+	def home
+	end
+	
+	def help
+	end
+
+  def login
   end
 end
--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,8 +1,8 @@
-Rails.application.routes.draw do
-  root 'static_pages#home'
+	Rails.application.routes.draw do
 
-  get 'static_pages/help'
-  
-
-
-end
+	 root 'static_pages#home'
+	 get 'static_pages/login'
+	 get 'help'    => 'static_pages#help'
+	 get 'login'    => 'static_pages#login'
+	  
+	end
